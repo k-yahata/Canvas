05f71ac246240e9f84aeaec969cf7601059d8371
k-yahata <43901946+k-yahata@users.noreply.github.com>
2023-01-18 13:16:01 +0900

    test_Point2D.cpp を更新

---
+++ test/test_Point2D.cpp
@@ -0,0 +1,17 @@
+#include "Point2D.hpp"
+#include <iostream>
+using namespace std;
+int main(){
+
+    //
+    Point2D p1( 0.0f, 1.0f );
+    
+    // abs()
+
+    // inner product
+
+    // normalize
+
+
+    return 0;
+}
